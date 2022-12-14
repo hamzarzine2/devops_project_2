016651878569140237adaeabf7df487e6ba3e064
NJKatorz <nathanesakwa@gmail.com>
2022-12-14 12:14:47 +0100

    style(all): fixing code prettier, eslint

--- __tests__/reviewAdd.test.js
+++ __tests__/reviewAdd.test.js
@@ -1,41 +1,38 @@
-const reviewAdd=require('../services/reviewAdd');
+const reviewAdd = require("../services/reviewAdd");
 
-describe("test of the method review add that should only allow planets with uppercases and '-' OR '.'",()=> {
-    it("should return false if the string is empty",()=> {
+describe("test of the method review add that should only allow planets with uppercases and '-' OR '.'", () => {
+    it("should return false if the string is empty", () => {
         const result = reviewAdd("");
         expect(result).toBe(false);
     });
 
-    it("should return false if the string has a lowercase character",()=> {
+    it("should return false if the string has a lowercase character", () => {
         const result = reviewAdd("TRaPPist");
         expect(result).toBe(false);
     });
-    
-    it("should return true if the string contains only upperCase character and number",()=> {
+
+    it("should return true if the string contains only upperCase character and number", () => {
         const result = reviewAdd("TRAPPIST25");
         expect(result).toBe(true);
     });
 
-    it("should return false if the string contains a special character different to '-' or '.'",()=> {
+    it("should return false if the string contains a special character different to '-' or '.'", () => {
         const result = reviewAdd("TRAPPIST-$25");
         expect(result).toBe(false);
     });
 
-    it("should return true if the string has only uppercase character, numbers or special character equals to '-'",()=> {
+    it("should return true if the string has only uppercase character, numbers or special character equals to '-'", () => {
         const result = reviewAdd("TRAPPIST-25");
         expect(result).toBe(true);
     });
 
-    it("should return true if the string has only uppercase character, numbers or special character equals to '-' or '.'",()=> {
+    it("should return true if the string has only uppercase character, numbers or special character equals to '-' or '.'", () => {
         const result = reviewAdd("TRAPPIST-2.5");
         expect(result).toBe(true);
     });
 
-    it("should return false if presence of non permisible characters",()=> {
+    it("should return false if presence of non permisible characters", () => {
         const result = reviewAdd("Trappist$*****01****-00");
         expect(result).toBe(false);
     });
-
-
 });
-
